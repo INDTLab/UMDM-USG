8dd0d9488175aac01586d90397c0744396822eb7
xiaoxiaodadada <125524021+xiaoxiaodadada@users.noreply.github.com>
2026-06-05 14:57:26 +0800

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+# from .nets.jodi_loss import (
+from .nets.UMDM_USG import (
+# from .nets.me0815 import (
+# from .nets.jodi import (
+    MAM,
+    UMDM,
+    P1_D28,
+    P2_D28,
+    P4_D28,
+    P1_D20,
+    P2_D20,
+)
